cb777c32f22874ae9faca0fe15f7839cc368df52
ziyuwwang <ziyuw0824@gmail.com>
2026-02-27 19:58:06 -0500

    add files

---
+++ scripts/train_flowers.sh
@@ -0,0 +1,8 @@
+python train.py \
+--dataset "flowers" \
+--log_dir "./logs/flowers" \
+--thick 1e-4 \
+--num_parts 16 \
+--block 8 \
+--missing 0.9 \
+--use_alpha True
---
+++ scripts/train_h36m.sh
@@ -0,0 +1,8 @@
+python train.py \
+--dataset "h36m" \
+--log_dir "./logs/h36m" \
+--thick 5e-5 \
+--num_parts 16 \
+--block 16 \
+--missing 0.8 \
+--use_alpha True
